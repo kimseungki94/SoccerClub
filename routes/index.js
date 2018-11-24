3bd045c4e3ac3ba8726dcb19e913c9a4c139ed04
kimseungki94 <skkim0832@gmail.com>
2018-11-25 08:50:06 +0900

    login page activate, but design is poor.. I will try to fix it

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { a: 'Hello' });
+  res.render('index', 
+  { title: '사커클럽', 
+    content: '오신걸환영합니다'
+  });
 });
 
 module.exports = router;
