f8c9d42fa6728509558f222e75d5bbcc68bd68b4
kimseungki94 <skkim0832@gmail.com>
2018-11-27 22:09:31 +0900

    signout activate

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signinRouter = require('./routes/account/signin');
+var signoutRouter = require('./routes/account/signout');
 var practiveRouter = require('./routes/practice/practice')
+
 var app = express();
 
 // view engine setup
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/account/signin', signinRouter);
+app.use('/account/signout', signoutRouter);
 app.use('/practice/practice', practiveRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
